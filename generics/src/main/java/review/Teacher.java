fb0cc76bcdbcc0d7160abf8136e0ce90da01805d
reyhaneterzioglu <terzioglureyhane@gmail.com>
2023-08-05 19:19:23 +0200

    Implemented Inheritance with Person class

--- generics/src/main/java/review/Teacher.java
+++ generics/src/main/java/review/Teacher.java
@@ -1,37 +1,9 @@
 package review;
 
-public class Teacher {
+public class Teacher extends Person{
 
-    private String name;
-
-    private int id;
 
     public Teacher(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Teacher{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                '}';
+        super(name, id);
     }
 }
