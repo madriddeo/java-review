fc39a9771e09318191ffcf1a44ab622a8f8ad26d
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-08-12 18:14:05 +0200

    behavior parameterization review - part 2

--- lambda/src/main/java/review/CarTest.java
+++ lambda/src/main/java/review/CarTest.java
@@ -1,5 +1,8 @@
 package review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarTest {
 
     public static void main(String[] args) {
@@ -11,7 +14,67 @@ public class CarTest {
 //        Car car = Car.builder().make("Toyota").build();
 //        System.out.println(car);
 
+        List<Car> carList = new ArrayList<>();
 
+        carList.add(new Car("Toyota", 140, 1998));
+        carList.add(new Car("Audi", 200, 2008));
+        carList.add(new Car("BMW", 210, 2020));
+        carList.add(new Car("Nissan", 150, 2018));
+
+//        System.out.println("Fast Cars:");
+//        System.out.println(filterFastCars(carList));
+//
+//        System.out.println("New Cars:");
+//        System.out.println(filterNewCars(carList));
+
+        System.out.println("Fast Cars:");
+        System.out.println(filter(carList, new CarFastPredicate()));
+
+        System.out.println("New Cars:");
+        System.out.println(filter(carList, new CarNewPredicate()));
+
+
+    }
+
+//    private static List<Car> filterFastCars(List<Car> cars) {
+//
+//        List<Car> filteredCars = new ArrayList<>();
+//
+//        for (Car car : cars) {
+//            if (car.getTopSpeed() > 160) {
+//                filteredCars.add(car);
+//            }
+//        }
+//
+//        return filteredCars;
+//
+//    }
+//
+//    private static List<Car> filterNewCars(List<Car> cars) {
+//
+//        List<Car> filteredCars = new ArrayList<>();
+//
+//        for (Car car : cars) {
+//            if (car.getYear() > 2015) {
+//                filteredCars.add(car);
+//            }
+//        }
+//
+//        return filteredCars;
+//
+//    }
+
+    private static List<Car> filter(List<Car> cars, CarPredicate carPredicate) {
+
+        List<Car> filteredCars = new ArrayList<>();
+
+        for (Car car : cars) {
+            if (carPredicate.test(car)) {
+                filteredCars.add(car);
+            }
+        }
+
+        return filteredCars;
 
     }
 
